826e38427b4fbb90f342a4cb689e101eac744016
drkarthiklaxmanai <drkarthiklaxmanai@gmail.com>
2026-08-25 02:33:08 +0530

    feat(auth): route participant through OTP gate

--- apps/participant/src/main.tsx
+++ apps/participant/src/main.tsx
@@ -1,5 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
+import OtpGate from './OtpGate'
 
-createRoot(document.getElementById('root')!).render(<StrictMode><App /></StrictMode>)
+createRoot(document.getElementById('root')!).render(<StrictMode><OtpGate /></StrictMode>)
